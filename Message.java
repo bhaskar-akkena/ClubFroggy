4885cf08a545e1bb9cedf1f4fceb33a71eafa270
JoshArq <95379445+JoshArq@users.noreply.github.com>
2021-12-04 22:43:22 -0500

    Adding the Message Object

---
+++ Message.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class Message implements Serializable{
+ private String name;
+ private String contents;
+ private String color;
+ private static final long serialVersionUID = 01L;
+
+ //Constructor
+ public Message(String clr, String n, String cont){
+  color=clr;
+  name=n;
+  contents=cont;
+ }//end constructor
+ 
+ //accessor and mutators
+ public String getName(){return name;}
+ public String getContents(){return contents;}
+ public String getColor(){return color;}
+ public void setName(String n){name=n;}
+ public void setContents(String c){contents=c;}
+ public void setColor(String c){color=c;}
+}//end class
