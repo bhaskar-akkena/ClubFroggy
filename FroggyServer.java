2c5d722e8894775486f88665bfdcf4f16f520e6d
JoshArq <95379445+JoshArq@users.noreply.github.com>
2021-12-06 23:27:27 -0500

    It still breaks when reading files :{

--- FroggyServer.java
+++ FroggyServer.java
@@ -7,8 +7,8 @@ import javafx.application.Platform;
 
 public class FroggyServer{
  //Objects+Variables
- private ObjectInputStream ois=null;
- private ObjectOutputStream oos=null;
+ private DataInputStream dis=null;
+ private DataOutputStream dos=null;
  private TextArea taLog;
  private TreeMap<String, Account> map = new TreeMap<String, Account>();
  private Account tempAccount;
@@ -28,11 +28,11 @@ public class FroggyServer{
   try{
    //if the file exists
    if(ACCOUNT_FILE.exists()){
-    ois=new ObjectInputStream(new FileInputStream(ACCOUNT_FILE));
+    dis=new DataInputStream(new FileInputStream(ACCOUNT_FILE));
     
     //put accounts into the TreeMap until it runs out of accounts.
     while(true){
-     tempAccount=(Account)ois.readObject();
+     tempAccount=new Account(dis.readUTF(), dis.readUTF(), dis.readUTF());
      map.put(tempAccount.getName(), tempAccount);
     }//end while
    }//end if
@@ -40,7 +40,7 @@ public class FroggyServer{
   
   //displays the machine's ip address
   try{
-   log("Running Server with IP address "+InetAddress.getLocalHost());
+   log("Running Server with IP address "+InetAddress.getLocalHost()+"\n");
   }catch(UnknownHostException uhe){log("UnknownHostException "+uhe);
    return;
   }//end try/catch
@@ -68,20 +68,23 @@ public class FroggyServer{
      ACCOUNT_FILE.createNewFile();
     
     //writes each account to the file
-    oos = new ObjectOutputStream(new FileOutputStream(ACCOUNT_FILE));
-    for(Map.Entry<String, Account> entry : map.entrySet())
-     oos.writeObject(entry.getValue());
+    dos = new DataOutputStream(new FileOutputStream(ACCOUNT_FILE));
+    for(Map.Entry<String, Account> entry : map.entrySet()){
+     dos.writeUTF(entry.getValue().getName());
+     dos.writeUTF(entry.getValue().getPassword());
+     dos.writeUTF(entry.getValue().getColor());
+    }
     
     //closes and flushes the ObjectOutputStream
-    oos.flush();
-    oos.close();
+    dos.flush();
+    dos.close();
    }catch(IOException ioe){return;}
   }//end if
  }//end save
  
  //logs in a user
  public String doLogIn(String userName, String password){
-  //synchronized on the hashmap
+  //synchronized on the treemap
   synchronized(map){
    //if the username is in the map
    if(map.containsKey(userName)){
@@ -109,6 +112,7 @@ public class FroggyServer{
     return "ac";
    }
   }//end synchronized
+  
  }//end doCreateAccount
 }//end FroggyServer
 
@@ -194,7 +198,7 @@ class ClientThread extends Thread{
  
  public void run(){
   //Informs the user that there's a connection
-  base.log("User Connected");
+  base.log("User Connected\n");
   try{
    //input and output streams
    in=new ObjectInputStream(cSocket.getInputStream());
@@ -278,6 +282,7 @@ class ClientThread extends Thread{
   }catch(IOException ioe){base.log("IOException "+ioe);
    return;
   }//end try/catch
+
  }//end doCreateAccount
  
  //method to log in a user
@@ -289,6 +294,7 @@ class ClientThread extends Thread{
  
    //Calls a method that's in a better place
    request=base.doLogIn(userName, password);
+   
    
    //synchronized on the outputstream
    synchronized(out){
@@ -330,4 +336,5 @@ class ClientThread extends Thread{
    }//end try/catch
   }//end if
  }//end doSendMesssage
+}//end ClientThread
 }//end ClientThread
