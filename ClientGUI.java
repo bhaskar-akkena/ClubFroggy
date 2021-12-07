64aafbe9643b64672fb58f197a125aaa3b1fe839
bhaskar-akkena <95503941+bhaskar-akkena@users.noreply.github.com>
2021-12-06 22:59:16 -0500

    Update ClientGUI.java

--- ClientGUI.java
+++ ClientGUI.java
@@ -1,16 +1,20 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.ComboBox;
 
+import java.io.FileInputStream;
 //A GUI for clients to connect with
 public class ClientGUI extends Application implements EventHandler<ActionEvent>{
  //GUI elements
